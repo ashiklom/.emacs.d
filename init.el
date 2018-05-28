fb516cb9296acbcc76bc274e120cd1dfb71027bf
Alexey Shiklomanov <ashiklom@bu.edu>
2018-05-28 15:34:20 -0400

    Add quelpa package
    For installing packages from source.

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;; Custom file. Mostly, I avoid using custom in favor of ~setq~.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
@@ -28,6 +29,10 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile (require 'use-package))
+
+;; For installing packages from source (e.g. GitHub)
+(use-package quelpa-use-package
+  :ensure t)
 
 ;; Global settings
 (setq inhibit-splash-screen t
