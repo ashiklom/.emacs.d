5cc78994196568ead894bf866fb239d6f0136a1c
Alexey Shiklomanov <ashiklom@bu.edu>
2018-05-29 18:55:51 -0400

    ESS: Improve key bindings and other config

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -6,45 +6,71 @@
 ;;; Code:
 (use-package ess
   :ensure t
-  :mode ("\\.[RR]\\'" . ess-r-mode)
+  :mode ("\\.[RR]\\'" . r-mode)
   :init
   (add-to-list 'evil-emacs-state-modes 'inferior-ess-mode)
+  (add-to-list 'evil-emacs-state-modes 'ess-rdired-mode)
   (setq comint-move-point-for-output t)	; Scroll R buffer on output
   (setq ess-ask-for-ess-directory nil
 	ess-directory-function 'ans-r-file-here
 	ess-default-style 'RStudio
 	ess-use-company t)
   :config
+  (require 'ess-rutils)
   (add-to-list 'ess-R-font-lock-keywords
 	       '(ess-fl-keyword:fun-calls . t) t)
+  (ans/add-evil-maps 'ess-help-mode-map)
   :general
-  (:keymaps 'ess-mode-map
-	    :states 'normal
-	    :prefix "\\"
-	    "r f" 'ans-start-R
-	    "r q" 'ans-quit-R
-	    "l" 'ess-eval-line
-	    "d" 'ess-eval-line-and-step
-	    "f f" 'ess-eval-function
-	    "p p" 'ess-eval-paragraph
-	    "p d" 'ess-eval-paragraph-and-step
-	    "a a" 'ess-eval-buffer
-	    "a d" 'ess-eval-buffer-from-here-to-end
-	    "a s" 'ess-eval-buffer-from-beg-to-here
-	    "r h" 'ess-display-help-on-object
-	    "v i" 'ess-r-devtools-install-package
-	    "v d" 'ess-r-devtools-document-package
-	    "v l" 'ess-r-devtools-load-package)
-  (:states 'visual
-	   :keymaps 'ess-mode-map
-	   :prefix "\\"
-	   "s s" 'ess-eval-region)
-  (:states 'insert
-	   :keymaps 'ess-mode-map
-	   "M-m" (lambda() (interactive)(insert " %>%"))
-	   "M--" 'ess-insert-S-assign
-	   "C-c" (lambda() (interactive)(insert "#'")))
-  )
+  (general-def
+    :keymaps 'ess-mode-map
+    :states 'normal
+    :prefix "\\"
+    "r f" 'ans-start-R
+    "r q" 'ans-quit-R
+    "l" 'ess-eval-line
+    "d" 'ess-eval-line-and-step
+    "f f" 'ess-eval-function
+    "p p" 'ess-eval-paragraph
+    "p d" 'ess-eval-paragraph-and-step
+    "a a" 'ess-eval-buffer
+    "a d" 'ess-eval-buffer-from-here-to-end
+    "a s" 'ess-eval-buffer-from-beg-to-here
+    "r h" 'ess-display-help-on-object
+    "v i" 'ess-r-devtools-install-package
+    "v d" 'ess-r-devtools-document-package
+    "v l" 'ess-r-devtools-load-package
+    "r o" 'ess-rutils-objs)
+  (general-def
+    :states 'visual
+    :keymaps 'ess-mode-map
+    :prefix "\\"
+    "s s" 'ess-eval-region)
+  (general-def
+    :states 'insert
+    :keymaps 'ess-mode-map
+    "_" 'self-insert-command
+    "M-m" (lambda() (interactive)(insert " %>%"))
+    "M--" 'ess-insert-S-assign
+    "C-c" (lambda() (interactive)(insert "#'")))
+  (general-def
+    :keymaps 'ess-help-mode-map
+    :states 'emacs
+    "SPC" 'ans-leader-command)
+  (general-def
+    :keymaps 'ess-help-mode-map
+    :states 'emacs
+    :prefix "\\"
+    "r h" 'ess-display-help-on-object)
+  (general-def
+    :keymaps 'ess-rdired-mode-map
+    "j" 'ess-rdired-next-line
+    "k" 'ess-rdired-previous-line))
+
+(defun ans/inferior-ess-mode-setup ()
+  "My custom configuration for inferior-ess-mode."
+  (setq kill-buffer-query-functions (delq 'process-kill-buffer-query-function kill-buffer-query-functions)))
+
+(add-hook 'inferior-ess-mode-hook 'ans/inferior-ess-mode-setup)
 
 (defun ans-split-right-if-wide ()
   "Split the window to the right if there is sufficient space."
