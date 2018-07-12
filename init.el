1eb1b05d6bcdf79f4ef78f0868a9af11e613b5aa
Alexey Shiklomanov <ashiklom@bu.edu>
2018-07-12 13:50:42 -0400

    Don't byte-compile emacs config

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 ;;;
 ;;; Code:
 (let ((file-name-handler-alist nil)
-      (my-emacs-file (expand-file-name "configuration.elc" user-emacs-directory)))
+      (my-emacs-file (expand-file-name "configuration.el" user-emacs-directory)))
   ;; If config is precompiled, load it
   (if (file-exists-p my-emacs-file)
       (load-file my-emacs-file)
